c252b698f7c0e953ad7b670c642081a68df3c59c
AimlessCode <emin_baycan@outlook.com>
2022-02-05 11:26:43 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import com.emin.Kedi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Kedi kedi;
+
+         kedi =new Kedi();
+        kedi.ad="emin";
+
+        System.out.println("Kedi uyku"+ kedi.uyanık);
+
+
+    }
+}
---
+++ src/com/emin/Kedi.java
@@ -0,0 +1,24 @@
+package com.emin;
+public class Kedi {
+    // 1- Değişken
+    // 2- Methotlar
+    // 3- InnerClass
+
+       public int boy ;
+    public int kilo ;
+    public String ad ;
+public boolean  uyku=true;
+    public String uyanık;
+
+    public void uyanık()
+    {
+        uyku=false;
+    }
+    public void uykulu()
+    {
+        uyku=true;
+    }
+    
+
+
+}
